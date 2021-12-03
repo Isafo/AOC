36ea463161cc5c364d7d943942bf7693650ede47
Isafo <Isabelle.forsman@gmail.com>
2021-12-03 20:33:48 +0100

    d1 b done

--- src/bin/1b.rs
+++ src/bin/1b.rs
@@ -3,20 +3,26 @@ use std::fs;
 fn main() {
     println!("Hello World");
 
-    let input = fs::read_to_string("input/d1.txt").unwrap();
+    let input = fs::read_to_string("input/d1.txt")
+        .unwrap()
+        .lines()
+        .map(|line| line.parse::<i32>().unwrap())
+        .collect::<Vec<_>>();
 
     let mut prev_val = None;
     let mut increase_count = 0;
-    for line in input.lines() {
-        let val = line.parse::<i32>().unwrap();
+    for it in 2..input.len() {
+        let mut sum = input[it - 2];
+        sum += input[it - 1];
+        sum += input[it];
 
         if let Some(prev_val) = prev_val {
-            if val > prev_val {
+            if sum > prev_val {
                 increase_count += 1;
             }
         }
 
-        prev_val = Some(val);
+        prev_val = Some(sum);
     }
 
     println!("{}", increase_count);
